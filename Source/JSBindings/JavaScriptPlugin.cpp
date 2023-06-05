8cc630df1896fb52a004445038a6f91ebe41824c
Ruben Gomes <rbnpontes@gmail.com>
2023-06-05 20:20:55 -0300

    feat: setup bindings plugin

--- Source/JSBindings/JavaScriptPlugin.cpp
+++ Source/JSBindings/JavaScriptPlugin.cpp
@@ -1,9 +1,15 @@
 #include "JavaScriptPlugin.h"
-
+#include "Bindings.h"
+#include <Urho3D/JavaScript/JavaScriptSystem.h>
 extern "C" URHO3D_EXPORT_API PluginApplication * PluginApplicationMain(Context * context)
 {
     PluginApplication* app = new JavaScriptPlugin(context);
     app->SetPluginName("JavaScriptBindingsPlugin");
     return app;
 }
-JavaScriptPlugin::JavaScriptPlugin(Context* context) : MainPluginApplication(context){}
+JavaScriptPlugin::JavaScriptPlugin(Context* context) : MainPluginApplication(context)
+{
+    JavaScriptSystem* system = GetSubsystem<JavaScriptSystem>();
+    Bindings_setup(static_cast<duk_context*>(system->GetJSCtx()));
+    URHO3D_LOGDEBUG("JavaScript Bindings has been initialized.");
+}
--- Source/Tools/JSPlayer/JSPlayer.cpp
+++ Source/Tools/JSPlayer/JSPlayer.cpp
@@ -4,6 +4,10 @@
 #include <Urho3D/JavaScript/JavaScriptAsset.h>
 #include <Urho3D/Engine/EngineDefs.h>
 #include <Urho3D/Input/Input.h>
+#include <Urho3D/Plugins/PluginManager.h>
+#include <Urho3D/Plugins/ModulePlugin.h>
+#include <Urho3D/IO/FileSystem.h>
+
 namespace Urho3D
 {
     JSPlayer::JSPlayer(Context* context) : Application(context)
@@ -26,9 +30,22 @@ namespace Urho3D
 
     void JSPlayer::Setup() {
         URHO3D_LOGDEBUG("Setup JSPlayer");
+        FileSystem* fs = context_->GetSubsystem<FileSystem>();
+
+        PluginApplication::RegisterStaticPlugins();
+
     }
     void JSPlayer::Start() {
         URHO3D_LOGDEBUG("Starting JSPlayer");
+        FileSystem* fs = context_->GetSubsystem<FileSystem>();
+
+        ModulePlugin* jsBindingsPlugin = new ModulePlugin(context_);
+        jsBindingsPlugin->SetName("JSBindings");
+
+        auto pluginManager = GetSubsystem<PluginManager>();
+        pluginManager->AddDynamicPlugin(jsBindingsPlugin);
+        pluginManager->StartApplication();
+
         Input* input = GetSubsystem<Input>();
         input->SetMouseMode(MM_FREE);
         input->SetMouseVisible(true);
