6999d56bd4a193b6f3ec014ae3390b60c728f91c
Ruben Gomes <rbnpontes@gmail.com>
2023-06-18 14:33:52 -0300

    feat: add component register

--- Source/Urho3D/JavaScript/JavaScriptSystemBindings.cpp
+++ Source/Urho3D/JavaScript/JavaScriptSystemBindings.cpp
@@ -6,6 +6,8 @@
 
 #define JS_HIDDEN_COMPONENT_TYPE DUK_HIDDEN_SYMBOL("__type")
 #define JS_HIDDEN_COMPONENT_CTOR DUK_HIDDEN_SYMBOL("__ctor")
+#define JS_OBJ_FACTORIES_PROP "__factories"
+
 namespace Urho3D
 {
     duk_ret_t GetSubsystem_Call(duk_context* ctx)
@@ -117,7 +119,63 @@ namespace Urho3D
         duk_push_pointer(ctx, typeInfo);
         duk_put_prop_string(ctx, -2, JS_HIDDEN_COMPONENT_TYPE);
 
+        duk_push_c_function(ctx, [](duk_context* ctx) {
+            duk_push_current_function(ctx);
+            if (duk_get_prop_string(ctx, -1, JS_HIDDEN_COMPONENT_TYPE)) {
+                TypeInfo* typeInfo = static_cast<TypeInfo*>(duk_get_pointer(ctx, -1));
+                delete typeInfo;
+            }
+            return 0;
+        }, 0);
+        duk_push_pointer(ctx, typeInfo);
+        duk_put_prop_string(ctx, -2, JS_HIDDEN_COMPONENT_TYPE);
+        duk_set_finalizer(ctx, -2);
         return 1;
+    }
+
+    SharedPtr<Object> Component_Factory(const TypeInfo* type, Context* context)
+    {
+        duk_context* ctx = static_cast<duk_context*>(JavaScriptSystem::GetJSCtx());
+        duk_idx_t top = duk_get_top(ctx);
+        duk_push_global_stash(ctx);
+        duk_get_prop_string(ctx, -1, JS_OBJ_FACTORIES_PROP);
+
+        URHO3D_ASSERTLOG(
+            duk_get_prop_string(ctx, -1, type->GetTypeName().c_str()),
+            Format("can´t create object '{}' type. it seems this type has not yet registered.", type->GetTypeName())
+        );
+
+        duk_new(ctx, 0);
+        SharedPtr<Object> result(static_cast<Object*>(rbfx_get_instance(ctx, -1)));
+
+        // reset allocated stack.
+        duk_pop_n(ctx, duk_get_top(ctx) - top);
+        return result;
+    }
+    duk_ret_t ComponentRegister_Call(duk_context* ctx)
+    {
+        duk_require_function(ctx, 0);
+
+        if (!duk_get_prop_string(ctx, 0, JS_HIDDEN_COMPONENT_TYPE))
+        {
+            duk_push_error_object(ctx, DUK_ERR_TYPE_ERROR, "invalid js component type. must use rbfx.component(function() { ... }, 'Component') as parameter.");
+            return duk_throw(ctx);
+        }
+
+        TypeInfo* type = static_cast<TypeInfo*>(duk_get_pointer(ctx, -1));
+
+        duk_push_global_stash(ctx);
+        duk_get_prop_string(ctx, -1, JS_OBJ_FACTORIES_PROP);
+
+        duk_dup(ctx, 0);
+        duk_put_prop_string(ctx, -2, type->GetTypeName().c_str());
+        duk_pop_2(ctx);
+
+        Context* engineCtx = JavaScriptSystem::GetContext();
+        ObjectReflection* componentReflection = engineCtx->AddFactoryReflection<JavaScriptComponent>("Component/JavaScript");
+        componentReflection->SetObjectFactory(Component_Factory);
+
+        return 0;
     }
 
     void JavaScript_SetupSystemBindings(duk_context* ctx)
@@ -130,6 +188,17 @@ namespace Urho3D
         duk_push_c_function(ctx, ComponentWrap_Call, 2);
         duk_put_prop_string(ctx, -2, "component");
 
+        duk_push_c_function(ctx, ComponentRegister_Call, 1);
+        duk_put_prop_string(ctx, -2, "registerComponent");
+
         duk_put_global_string(ctx, "rbfx");
+        duk_pop(ctx);
+
+        // setup factories stash table
+        duk_push_global_stash(ctx);
+        duk_push_object(ctx);
+        duk_put_prop_string(ctx, -2, JS_OBJ_FACTORIES_PROP);
+
+        duk_pop(ctx);
     }
 }
