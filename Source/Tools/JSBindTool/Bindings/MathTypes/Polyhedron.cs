5eb383bb940212b51ab3baf24ba4b2dd12b1b0e7
Ruben Gomes <rbnpontes@gmail.com>
2023-08-05 18:27:19 -0300

    feat: add polyhedron bindings

--- Source/Tools/JSBindTool/Bindings/MathTypes/Polyhedron.cs
+++ Source/Tools/JSBindTool/Bindings/MathTypes/Polyhedron.cs
@@ -11,11 +11,45 @@ namespace JSBindTool.Bindings.MathTypes
     [Include("Urho3D/Math/Polyhedron.h")]
     public class Polyhedron : PrimitiveObject
     {
+        [Variable("faces_")]
+        public Vector<Vector<Vector3>> Faces = new Vector<Vector<Vector3>>();
+        [PropertyMap("Empty")]
+        public bool Empty { get => false; }
         public Polyhedron() : base(typeof(Polyhedron)) { }
 
         [Constructor]
         public void Constructor() { }
         [Constructor]
         public void Constructor(Vector<Vector<Vector3>> faces) { }
+        [Constructor]
+        public void Constructor(BoundingBox bbox) { }
+        [Constructor]
+        public void Constructor(Frustum frustum) { }
+
+        [Method]
+        public void Define(BoundingBox bbox) { }
+        [Method]
+        public void Define(Frustum frustum) { }
+        [Method]
+        public void AddFace(Vector3 v0, Vector3 v1, Vector3 v2) { }
+        [Method]
+        public void AddFace(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3) { }
+        [Method]
+        public void AddFace(Vector<Vector3> vertices) { }
+        [Method]
+        public void Clip(Plane plane) { }
+        [Method]
+        public void Clip(BoundingBox box) { }
+        [Method]
+        public void Clip(Frustum frustum) { }
+        [Method]
+        public void Clear() { }
+        [Method]
+        public void Transform(Matrix3 transform) { }
+        [Method]
+        public void Transform(Matrix3x4 transform) { }
+
+        public Polyhedron Transformed(Matrix3 transform) => new Polyhedron();
+        public Polyhedron Transformed(Matrix3x4 transform) => new Polyhedron();
     }
 }
