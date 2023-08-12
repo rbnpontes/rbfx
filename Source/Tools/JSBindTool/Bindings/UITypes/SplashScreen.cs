cd576a6ec85035c64d950c2815005203d59975e6
Ruben Gomes <rbnpontes@gmail.com>
2023-08-12 11:54:48 -0300

    fix: splashscreen scene include

--- Source/Tools/JSBindTool/Bindings/UITypes/SplashScreen.cs
+++ Source/Tools/JSBindTool/Bindings/UITypes/SplashScreen.cs
@@ -6,6 +6,7 @@ using JSBindTool.Core.Annotations;
 
 namespace JSBindTool.Bindings.UITypes
 {
+    [Include("Urho3D/Scene/Scene.h")] // required to build correctly SplashScreen
     [Include("Urho3D/UI/SplashScreen.h")]
     public class SplashScreen : ApplicationState
     {
